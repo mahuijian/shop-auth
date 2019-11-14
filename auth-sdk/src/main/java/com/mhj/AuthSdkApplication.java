b3a142dffaee4e03dcd3a9c104891f1008b63224
mahuijian <huijianm.rr@moguyun.com>
2019-11-14 16:25:38 +0800

    feat: 添加auth-sdk

---
+++ auth-sdk/src/main/java/com/mhj/AuthSdkApplication.java
@@ -0,0 +1,13 @@
+package com.mhj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSdkApplication.class, args);
+	}
+
+}
