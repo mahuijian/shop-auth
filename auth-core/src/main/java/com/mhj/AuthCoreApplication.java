7547b2bb57d6adffca80b74ada952c3d67b17a88
mahuijian <huijianm.rr@moguyun.com>
2019-11-14 16:53:47 +0800

    feat: 添加auth-core

---
+++ auth-core/src/main/java/com/mhj/AuthCoreApplication.java
@@ -0,0 +1,13 @@
+package com.mhj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthCoreApplication.class, args);
+	}
+
+}
---
+++ auth-core/src/test/java/com/mhj/AuthCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
